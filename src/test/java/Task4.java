40c903a55e89c72eb1e983484cef112fa8b7bdc3
Gvantsa Misheladze <gmisheladze@credo.ge>
2024-11-10 00:45:24 +0400

    task_4_Gvantsa

---
+++ src/test/java/Task4.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Task4 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("dog", "cat", "elephant", "tiger", "lion", "ant");
+        words.stream()
+                .filter(word -> word.length() > 4)
+                .map(String::toLowerCase)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
